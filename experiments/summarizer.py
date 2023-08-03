ea83330f3912d6ac5d3bd93501db4efb4826dfc7
Gabriel Mukobi <gabrielmukobi@gmail.com>
2023-08-03 11:15:59 +0100

    Start summarizer class

---
+++ experiments/summarizer.py
@@ -0,0 +1,22 @@
+"""Summarize message history to condense prompts."""
+
+from diplomacy import Game, Power
+
+from backends import OpenAIChatBackend
+
+import constants
+
+
+class OpenAISummarizer:
+    """Summarize message history to condense prompts."""
+
+    def __init__(self, model_name: str):
+        self.backend = OpenAIChatBackend(model_name)
+
+    def summarize(
+        self,
+        game: Game,
+        power: Power,
+    ) -> str:
+        """Summarize a list of messages."""
+        raise NotImplementedError
