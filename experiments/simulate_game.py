f8f42d6532e3fe59101c5a9307a58d64cc12718a
Gabriel Mukobi <gabrielmukobi@gmail.com>
2023-09-09 12:08:43 -0700

    Fix typo with not printing stuff on wandb runs

--- experiments/simulate_game.py
+++ experiments/simulate_game.py
@@ -306,8 +306,8 @@ def main():
                 now = datetime.now()
                 current_time = now.strftime("%Y-%m-%d %H:%M:%S")
                 agent_log_string = f"⚙️  {current_time} {power_name} {game.get_current_phase()} Round {message_round}: Agent {agent} took {agent_response.completion_time_sec:.2f}s to respond."
-                if not isinstance(wandb.run.mode, wandb.sdk.lib.disabled.RunDisabled):
-                    agent_log_string += "\nReasoning: {agent_response.reasoning}\nOrders: {agent_response.orders}\nMessages: {agent_response.messages}"
+                if isinstance(wandb.run.mode, wandb.sdk.lib.disabled.RunDisabled):
+                    agent_log_string += f"\nReasoning: {agent_response.reasoning}\nOrders: {agent_response.orders}\nMessages: {agent_response.messages}"
                 utils.log_info(
                     logger,
                     agent_log_string,
@@ -350,7 +350,7 @@ def main():
                     if valid_order_ratio is not None
                     else np.NaN
                 )
-                if not isinstance(wandb.run.mode, wandb.sdk.lib.disabled.RunDisabled):
+                if isinstance(wandb.run.mode, wandb.sdk.lib.disabled.RunDisabled):
                     utils.log_info(
                         logger,
                         f"✔️  {power_name} valid orders: {num_valid_orders}/{num_orders} = {valid_order_display_percent:.2f}%"
